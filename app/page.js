1b197b1e2d298277afa2418934ec59d17cceac8c
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2023-10-16 20:59:28 +0100

    starter

---
+++ app/page.js
@@ -0,0 +1,10 @@
+
+import styles from '../styles/page.module.scss'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+       <h1>Hello world</h1> 
+    </main>
+  )
+}
